74439ff84dcea94a9e424ecc40ac7c7deac28ba4
Nilton Carlos <niltonfirminofs@gmail.com>
2021-09-26 16:11:47 -0300

    initial

---
+++ lib/app/appwidget.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'models/splash.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
